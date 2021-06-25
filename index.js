ad22830ca837e9b81fa3df5dbe7271c4c452c915
niwolos <hansguempert@gmx.de>
2021-06-25 14:33:10 +0200

    Implemented level 2 middleware exercise features

--- index.js
+++ index.js
@@ -14,17 +14,34 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "index.html");
 })
 
-app.post("/upload-profile-pic", upload.single('profile_pic'), (req, res, next) => {
+app.post("/upload-profile-pic", upload.single('profile_pic'), (req, res) => {
     try {
         console.log({ file: req.file });
         if (!req.file) {
-            res.status(400).send("please upload the picture");
+            res.status(400).send("Please upload an image with either of these formats: jpeg/jpg/png/gif");
         } else {
             res.send(`<p>The profile pic you just uploaded:</p><img src="/uploads/${req.file.filename}" />`)
         }
     } catch (error) {
-        console.log({ e: error })
+        console.error({ e: error })
     }
+})
+
+app.post("/upload-cat-pics", upload.array('cat_pics'), (req, res) => {
+    try {
+        console.log({ files: req.files});
+        let htmlResult = ``;
+        let idx = 1;
+        for (const cat_pic in req.files) {
+            console.log(req.files[cat_pic])
+            htmlResult +=`<p>#${idx} of your cat picture uploads:</p><img src="/uploads/${req.files[cat_pic].filename}" />`;
+            idx++;
+        }
+        res.send(htmlResult)
+    } catch (error){
+        console.error({ e: error })
+    }
+
 })
 
 app.listen(port, () => {
--- utils/uploadLogic.js
+++ utils/uploadLogic.js
@@ -1,45 +1,34 @@
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    },
-
-
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 const upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        // check mimetype property
-        // if (req.file.mimetype !== 'image/jpeg') {
-        //     cb(null, false)
-        // }
-
-        const regex = /\.(jpg | jpeg | png | gif)$/igm
-        try {
-
-            if (file.originalname.match(regex)) {
-                console.log("validfile")
-                cb(null, true)
-            } else {
-                console.log("notvalidfile")
-                cb(null, false)
-            }
-        }
-        catch (e) { cb(new Error('I don\'t have a clue!')) }
-
-        // To accept the file pass `true`, like so:
-
-        // You can always pass an error if something goes wrong:
-
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const rePattern = new RegExp(/\.(jpg|jpeg|png|webp|gif)$/gim);
+    try {
+      if (file.originalname.match(rePattern)) {
+        console.log(`File ending is valid: ${path.extname(file.originalname)}`);
+        cb(null, true);
+      } else {
+        console.log(`File ending invalid: ${path.extname(file.originalname)}`);
+        cb(null, false);
+      }
+    } catch (e) {
+      cb(new Error("Error when trying to filter file endings!"));
     }
-})
-
-
+  },
+});
 
 module.exports = upload;
